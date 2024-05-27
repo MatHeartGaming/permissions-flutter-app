0fd6761dab1d33f9b4dacc93f370baeaf14cbe7e
Matteo Buompastore <matteo.buompastore088@gmail.com>
2024-05-27 14:36:43 +0200

    Sensors completed

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -5,3 +5,5 @@ export 'permissions/permissions_provider.dart';
 export 'sensors/gyroscope_provider.dart';
 export 'sensors/acelerometer_providers.dart';
 export 'sensors/magnetometer_provider.dart';
+
+export 'compass/compass_provider.dart';
