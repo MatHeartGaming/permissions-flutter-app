48fc8e205aecc747ee0d0015c706af6c91fc3b28
Matteo Buompastore <matteo.buompastore088@gmail.com>
2024-05-28 15:16:39 +0200

    Biometric plugin configured. Added wrapper class and first auth provider

--- android/app/src/main/kotlin/it/matbuompy/miscelaneos/MainActivity.kt
+++ android/app/src/main/kotlin/it/matbuompy/miscelaneos/MainActivity.kt
@@ -1,6 +1,6 @@
 package it.matbuompy.miscelaneos
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
